9e524b20ae8a0300f486f398726eadb5f6a9ff43
Jovan Cvijanović <119356876+AStroCvijo@users.noreply.github.com>
2024-11-01 02:01:13 +0100

    Update argparser.py

--- utils/argparser.py
+++ utils/argparser.py
@@ -8,17 +8,15 @@ def arg_parse():
 
     # Model arguments
     parser.add_argument('-m',   '--model',             type=str,   default = 'LSTM',         help="Which model to use")
-
-    # LSTM model arguments
-    parser.add_argument('-bi',  '--bidirectional',     type=bool,  default = False,          help="Will the LSTM be bidirectional or not")
-
-    # Transformer model arguments
-    parser.add_argument('-nh',  '--number_heads',      type=int,   default = 8,              help="Number of head of the Transformer model")
-
-    # Shared LSTM and Transformer arguments
     parser.add_argument('-hs',  '--hidden_dim',        type=int,   default = 768,            help="Size of the models hidden layer")
     parser.add_argument('-ed',  '--embedding_dim',     type=int,   default = 768,            help="Size of the embedding space")
     parser.add_argument('-nl',  '--num_layers',        type=int,   default = 2,              help="Number of layers in the model")
+
+    # LSTM specific arguments
+    parser.add_argument('-bi',  '--bidirectional',     type=bool,  default = False,          help="Will the LSTM be bidirectional")
+
+    # Transformer specific arguments
+    parser.add_argument('-nh',  '--number_heads',      type=int,   default = 8,              help="Number of head of the Transformer model")
 
     # Training arguments
     parser.add_argument('-e',   '--epochs',            type=int,   default = 30,             help="Number of epochs in training")
