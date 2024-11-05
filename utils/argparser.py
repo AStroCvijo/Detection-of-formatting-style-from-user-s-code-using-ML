6bbd0e4d98a89937ce1e11644ddc6ce393b77137
Jovan Cvijanović <119356876+AStroCvijo@users.noreply.github.com>
2024-11-05 14:20:25 +0100

    Update argparser.py

--- utils/argparser.py
+++ utils/argparser.py
@@ -7,7 +7,7 @@ def arg_parse():
     parser = argparse.ArgumentParser()
 
     # Model arguments
-    parser.add_argument('-m',   '--model',             type=str,   default = 'LSTM',         help="Model to use (e.g., LSTM, Transformer, N-gram)")
+    parser.add_argument('-m',   '--model',             type=str,   default = 'Transformer',  help="Model to use (e.g., LSTM, Transformer, N-gram)")
     parser.add_argument('-hs',  '--hidden_dim',        type=int,   default = 768,            help="Size of the models hidden layer")
     parser.add_argument('-ed',  '--embedding_dim',     type=int,   default = 768,            help="Size of the embedding space")
     parser.add_argument('-nl',  '--num_layers',        type=int,   default = 1,              help="Number of layers in the model")
@@ -24,7 +24,7 @@ def arg_parse():
     parser.add_argument('-mn',   '--model_name',       type=str,   default = 'model',        help="Name of the model when being saved")
 
     # Data arguments
-    parser.add_argument('-sl',  '--sequence_length',   type=int,   default = 10,              help="Length of input sequences")
+    parser.add_argument('-sl',  '--sequence_length',   type=int,   default = 10,             help="Length of input sequences")
     parser.add_argument('-bs',  '--batch_size',        type=int,   default = 256,            help="Batch size for training")
 
     # Parse the arguments
